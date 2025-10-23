000b39c6e88bcc813a055263fc60f29df01cfbea
DiegoBueno207 <diegodossantosbueno@gmail.com>
2025-10-22 23:42:49 -0300

    Add files via upload

---
+++ meu_script.js
@@ -0,0 +1,60 @@
+const palavra = document.getElementById('B');
+const gif = document.getElementById('diavolo');
+
+palavra.addEventListener('click', () => {
+    gif.classList.toggle('mostrar-gif');
+
+    setTimeout(() => {
+
+    gif.classList.remove('mostrar-gif');
+    }, 500);
+});
+
+const palavra2 = document.getElementById('C');
+const gif2 = document.getElementById('diavolo');
+
+palavra2.addEventListener('click', () => {
+    gif2.classList.toggle('mostrar-gif');
+
+    setTimeout(() => {
+
+    gif2.classList.remove('mostrar-gif');
+    }, 500);
+});
+
+const palavra3 = document.getElementById('D');
+const gif3 = document.getElementById('diavolo');
+
+palavra3.addEventListener('click', () => {
+    gif3.classList.toggle('mostrar-gif');
+
+    setTimeout(() => {
+
+    gif2.classList.remove('mostrar-gif');
+    }, 500);
+});
+const palavra4 = document.getElementById('E');
+const gif4 = document.getElementById('diavolo');
+
+palavra4.addEventListener('click', () => {
+    gif4.classList.toggle('mostrar-gif');
+
+    setTimeout(() => {
+
+    gif4.classList.remove('mostrar-gif');
+    }, 500);
+});
+const palavra5 = document.getElementById('A');
+const gif5 = document.getElementById('diavolo');
+
+palavra5.addEventListener('click', () => {
+    gif5.classList.toggle('mostrar-gif');
+
+    setTimeout(() => {
+
+    gif5.classList.remove('mostrar-gif');
+    }, 500);
+});
+
+
+
